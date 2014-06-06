61e63449be6fe46eae2b2b69a9db9e29517aed8a
David Meister <thedavidmeister@gmail.com>
2014-06-07 01:51:53 +1000

    testing

--- tests/TruncateTest.php
+++ tests/TruncateTest.php
@@ -3,5 +3,6 @@ class TruncateTest extends PHPUnit_Framework_Testcase {
   public function testCanCountString {
     $string = 'foo bar';
     $this->assertTrue(FALSE);
+    $this->assertTrue(TRUE);
   }
 }
